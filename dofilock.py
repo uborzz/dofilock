b681a9977d3c06868c2a16263ce53a0b65f97dd2
Marcial Rodríguez <uborzz@gmail.com>
2016-10-12 16:06:20 +0200

    autoreset & ilvl info

--- dofilock.py
+++ dofilock.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import urllib
 import json
 import time
@@ -9,6 +12,12 @@ from datetime import datetime, date
 
 key = open('publickey').read()
 base = 'https://eu.api.battle.net/wow/character/dun-modr/'
+
+def charItems(name):
+    logkey = '?fields=items&locale=en_GB&apikey=' + key
+    url = base + name + logkey
+    info = json.loads(urllib.urlopen(url).read())
+    return info
 
 def charFeed(name):
     logkey = '?fields=feed&locale=en_GB&apikey=' + key
@@ -42,6 +51,7 @@ def lastW():
     lastWeds = actual.replace(hour=9,minute=0, second=0,microsecond=0)
     lastWeds = long(time.mktime(lastWeds.timetuple())-86400*gap)*1000
     # resetDate = long(time.mktime(resetDate.timetuple()))*1000
+    print "ULTIMA MIERCOLES: ", lastWeds
     return lastWeds
 
 def checkReset():
@@ -66,9 +76,9 @@ def clearData():
         json.dump(general, fp)
     return general
 
-def updateHTML(gen):
+def updateHTML(gen, lvls):
     htmlFile = open('datashow.html','w') # CAMBIAR DIRECTORIO!
-    message = "<h3> Dofilock: Info saves </h3> Informacion sobre saves de cada player, datos extraidos de la api de battle net, por lo que es posible que la informacion no este 100% correcta/actualizada ya que los updates de blizzard no se realizan cada instante, ademas, hay eventos que se pueden perder en el feed que nos proporcionan. Disculpas por la representacion tan chapucera, es lo mas rapido funcional, sera mejorado.<br>"
+    message = "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' /><h3> Dofilock: Info saves </h3> Informacion sobre saves de cada player, datos extraidos de la api de battle net, por lo que es posible que la informacion no este 100% correcta/actualizada ya que los updates de blizzard no se realizan cada instante, ademas, hay eventos que se pueden perder en el feed que nos proporcionan. Disculpas por la representacion tan chapucera, es lo mas rapido funcional, sera mejorado.<br>"
 
     message = message + "<br><br> Mythics: "
     messUpd = ""
@@ -88,23 +98,33 @@ def updateHTML(gen):
         messUpd = messUpd + "<br>" + repr(item) + ": " + repr(gen[2][item])
     message = message + messUpd
 
+    message = message + "<br><br> NOTA: Si vuestro navegador cachea, pulsad F5 para actualizar... Este doc es un estático generado por script, chapucillas everywhere"
     # message = message + "<br><br> Last updates: "
     # messUpd = ""
     # for item in upd:
     #     messUpd = messUpd + "<br>" + repr(item) + ": " + repr(upd[item])
     # message = message + messUpd
 
+    message = message + "<br><br> Member iLvls (Media máximo inventario - Media Equipado)"
+    messUpd = ""
+    for item in lvls:
+        messUpd = messUpd + "<br>" + repr(item) + ": " + repr(lvls[item])
+    message = message + messUpd
+
     htmlFile.write(message)
     htmlFile.close()
 
 # MODE = 'FIRST'
 MODE = 'MAN'
-# STEP = 'CLEAR'
-# STEP = 'RUN'
+
 
 # A base datos......
-miembros = ['Wizaras', 'Cuxulain', 'Lorzo', 'Nimro', 'Fudan', 'Palomitera', 'Odlarg', \
-            'Khaelan', 'Llonganisa', 'Racknnar', 'Stiwie', 'Vaironn', 'Uborzz']
+miembros = ['Wizaras', 'Cuxulain', 'Lorzo', 'Nimro', 'Fudan', 'Palomitera', 'Odlarg', 'Megainho', 'Wizote', \
+            'Khaelan', 'Llonganisa', 'Racknnar', 'Stiwie', 'Vaironn', 'Uborzz', 'Sàs', 'Yarako', 'Kaelan']
+ilvls = dict()
+
+for miembro in miembros:
+    ilvls[miembro] = [0, 0]
 
 if MODE == 'FIRST':
     clearData()
@@ -112,7 +132,6 @@ if MODE == 'FIRST':
     lastUpd = dict()
     # db.insert_one()
     lastWeds = lastW()
-    STEP = 'RUN'
     print 'salimos FIRST'
 
     MODE = 'MAN'
@@ -129,7 +148,6 @@ if MODE == 'MAN':
     lastUpd = dict()
     # db.insert_one()
     lastWeds = lastW()
-    STEP = 'RUN'
     print 'salimos MAN'
 
     flag = False
@@ -138,93 +156,105 @@ while 1:
     # db.find???
     if checkReset() == 1 and flag == False:
         flag = True
-        STEP = 'CLEAR'
+        clearData()
+        lastWeds = lastW()
+        print 'Limpiando...'
     elif checkReset() == 2 and flag == True:
         flag = False
-        print 'reset flag'
+        print 'Reset flag limpia...'
+
+    count = 0
+    for miembro in miembros:
+        try:
+
+            # MYTHICS...
+            a = charSttics(miembro)
+            count = count + 1
+            codigosInstanceApi = [2,5,8,11,16,17,20,23,26,27,28]
+            print '> Counting dofito', count, 'of', len(miembros), '.....', miembro
+            for i in codigosInstanceApi:
+                # fecha mayor que ultimo miercoles?
+                d = a['statistics']['subCategories'][5]['subCategories'][6]['statistics'][i]
+                actual = d['lastUpdated']#+3*86400*1000
+                # print d, lastWeds, date(actual), date(lastWeds)
+                if actual > lastWeds:
+                    try:
+                        c = json.dumps(d['name']).split('(Mythic ')[1][:-2]
+                        # print c
+                        if miembro not in instances[c]:
+                            instances[c].append(miembro)
+                    except:
+                        pass
+                # raw_input()
+            # lastUpd[miembro] = date(a['feed'][1]['timestamp'])
 
 
-    if STEP == 'RUN':
+            # RAIDS...
+            emerald = charProg(miembro)
 
-        count = 0
-        for miembro in miembros:
-            try:
-                a = charSttics(miembro)
-                count = count + 1
-                codigosInstanceApi = [2,5,8,11,14,16,17,20,23,26,27,28]
-                print '> Counting dofito', count, 'of', len(miembros), '...'
-                for i in codigosInstanceApi:
-                    # fecha mayor que ultimo miercoles?
-                    d = a['statistics']['subCategories'][5]['subCategories'][6]['statistics'][i]
-                    actual = d['lastUpdated']
-                    if  actual > lastWeds:
-                        try:
-                            c = json.dumps(a['name']).split('(Mythic ')[1][:-2]
-                            if miembro not in instances[c]:
-                                instances[c].append(miembro)
-                        except:
-                            pass
-                # lastUpd[miembro] = date(a['feed'][1]['timestamp'])
+            for n in range(7):
+                if emerald['progression']['raids'][35]['bosses'][n]['normalTimestamp'] > lastWeds:
+                    if miembro not in emeraldN[emerald['progression']['raids'][35]['bosses'][n]['name']]:
+                        emeraldN[emerald['progression']['raids'][35]['bosses'][n]['name']].append(miembro)
+                if emerald['progression']['raids'][35]['bosses'][n]['heroicTimestamp'] > lastWeds:
+                    if miembro not in emeraldH[emerald['progression']['raids'][35]['bosses'][n]['name']]:
+                        emeraldH[emerald['progression']['raids'][35]['bosses'][n]['name']].append(miembro)
 
+            # KEYSTONES...
 
+            # ITEM LEVEL...
+            items = charItems(miembro)
+            # print json.dumps(a)
+            lvlM=items['items']['averageItemLevel']
+            lvlE=items['items']['averageItemLevelEquipped']
+            # print lvlM, lvlE
 
-                emerald = charProg(miembro)
-
-                for n in range(7):
-                    if emerald['progression']['raids'][35]['bosses'][n]['normalTimestamp'] > lastWeds:
-                        if miembro not in emeraldN[emerald['progression']['raids'][35]['bosses'][n]['name']]:
-                            emeraldN[emerald['progression']['raids'][35]['bosses'][n]['name']].append(miembro)
-                    if emerald['progression']['raids'][35]['bosses'][n]['heroicTimestamp'] > lastWeds:
-                        if miembro not in emeraldH[emerald['progression']['raids'][35]['bosses'][n]['name']]:
-                            emeraldH[emerald['progression']['raids'][35]['bosses'][n]['name']].append(miembro)
-
-            except:
-                print miembro, 'failed'
+            ilvls[miembro][0] = lvlM
+            ilvls[miembro][1] = lvlE
 
 
-        print ' ~~~~~~~~~~~~ '
-        print 'Mythic Basicas:'
-
-        for instance in instances:
-            print instance, ': ', instances[instance]
-
-        print ' ~~~~~~~~~~~~ '
-        print 'The Emerald Dream - Normal:'
-
-        for instance in emeraldN:
-            print instance, ': ', emeraldN[instance]
-
-        print ' ~~~~~~~~~~~~ '
-        print 'The Emerald Dream - Heroic:'
-
-        for instance in emeraldH:
-            print instance, ': ', emeraldH[instance]
+        except:
+            print miembro, 'failed'
 
 
-        general = [instances, emeraldN, emeraldH]
-        with open('dataX.json', 'w') as fp:
-            json.dump(general, fp)
+    print ' ~~~~~~~~~~~~ '
+    print 'Mythic Basicas:'
 
-        # print ' ~~~~~~~~~~~~ '
-        # print "Betrayers detection"
-        # print "Betrayers de la semana en Mythic: ", betrayers
+    for instance in instances:
+        print instance, ': ', instances[instance]
 
-        # print ' ~~~~~~~~~~~~ '
-        # print 'Ultima lectura:'
-        #
-        # for miembro in miembros:
-        #     print miembro, lastUpd[miembro]
+    print ' ~~~~~~~~~~~~ '
+    print 'The Emerald Dream - Normal:'
+
+    for instance in emeraldN:
+        print instance, ': ', emeraldN[instance]
+
+    print ' ~~~~~~~~~~~~ '
+    print 'The Emerald Dream - Heroic:'
+
+    for instance in emeraldH:
+        print instance, ': ', emeraldH[instance]
 
 
-        updateHTML(general)
+    general = [instances, emeraldN, emeraldH]
+    with open('dataX.json', 'w') as fp:
+        json.dump(general, fp)
 
-        # db.update_one()
-        # if martes:
-        #     estado = 'CLEAR'
-        time.sleep(300)
+    # print ' ~~~~~~~~~~~~ '
+    # print "Betrayers detection"
+    # print "Betrayers de la semana en Mythic: ", betrayers
 
-    elif STEP == 'CLEAR':
-        clearData()
+    # print ' ~~~~~~~~~~~~ '
+    # print 'Ultima lectura:'
+    #
+    # for miembro in miembros:
+    #     print miembro, lastUpd[miembro]
 
-        STEP = 'RUN'
-        print 'salimos CLEAR'
+
+    updateHTML(general, ilvls)
+
+    # db.update_one()
+    # if martes:
+    #     estado = 'CLEAR'
+    time.sleep(300)
+
